cc7d246c23c418cb0369869c4550619ba1127c92
21clemence21 <depernetclemence@gmail.com>
2020-10-08 20:46:13 +0200

    add first configs for database and creation of db for user and roles

--- server.js
+++ server.js
@@ -8,7 +8,34 @@ var corsOptions = {
   origin: "http://localhost:8081"
 };
 
-app.use(cors(corsOptions));
+app .use(cors(corsOptions));
+
+const db = require("./app/models");
+const { INITIALLY_DEFERRED } = require("sequelize/types/lib/deferrable");
+db.sequelize.sync().then(() => {
+  initial();
+});
+
+function initial() {
+  Role.create({
+    id: 1,
+    name: "user"
+  });
+ 
+  Role.create({
+    id: 2,
+    name: "moderator"
+  });
+ 
+  Role.create({
+    id: 3,
+    name: "admin"
+  });
+}
+
+// for dev : db.sequelize.sync({ force: true }).then(() => {
+//  console.log("Drop and re-sync db.");
+// });
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
@@ -18,7 +45,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome to my application" });
 });
 
 // set port, listen for requests
